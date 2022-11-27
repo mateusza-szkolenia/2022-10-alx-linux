841d8b30e27f8654d72a2395f522ba9d3d8e3b4a
Mateusz Adamowski <m.adamowski@alx.pl>
2022-11-27 14:46:55 +0100

    elif

--- 25_elif.sh
+++ 25_elif.sh
@@ -5,12 +5,10 @@ read -p 'Podaj imię: ' imie
 if test "$imie" == "Mateusz"
 then
 	echo "Witaj, mistrzu"
+elif test -z "$imie"
+then
+	echo "Witaj, nieznajomy"
 else
-	if test -z "$imie"
-	then
-		echo "Witaj, nieznajomy"
-	else
-		echo "Witaj, $imie"
-	fi
+	echo "Witaj, $imie"
 fi
 

a91e4428adf63d7814e9c6350521c10b9598b5b2
Mateusz Adamowski <m.adamowski@alx.pl>
2022-11-27 14:45:55 +0100

    kopia

---
+++ 25_elif.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read -p 'Podaj imię: ' imie
+
+if test "$imie" == "Mateusz"
+then
+	echo "Witaj, mistrzu"
+else
+	if test -z "$imie"
+	then
+		echo "Witaj, nieznajomy"
+	else
+		echo "Witaj, $imie"
+	fi
+fi
+

90f942c2b59ce21c9d40c0253328f27973dce822
Mateusz Adamowski <m.adamowski@alx.pl>
2022-11-27 12:43:14 +0100

    arytmetyka

--- 18_arytmetyka.sh
+++ 18_arytmetyka.sh
@@ -3,6 +3,21 @@
 a=100
 b=200
 
+# zadziała, ale tak się nie robi:
 echo "Suma: $(( $a + $b ))"
+
+# poprawny zapis
 echo "Suma: $(( a + b ))"
+
+# Przykład, dalczego pierwszy zapis jest błędny:
+
+#
+echo "Suma: $(( $a + $b + $c ))"
+# (( 100 + 200 + )) # SYNTAX ERROR
+
+
+# poprawny zapis
+echo "Suma: $(( a + b + c ))"
+# (( 100 + 200 + 0 ))
+
 

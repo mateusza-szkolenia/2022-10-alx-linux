8b435ef9e245de0678939864283ecbbb59226d21
Mateusz Adamowski <m.adamowski@alx.pl>
2022-11-27 10:27:38 +0100

    wynik programu jako wartość

---
+++ 08_program.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Jestem zalogowany jako $(whoami)"
+echo "Dzisiaj jest $(date +%A) w miesiącu $(date +%B)"
+echo "Dzisiaj jest `date +%A` w miesiącu `date +%B`"
+
+# odwrotny apostrof 	`		(przy tyldzie, przed 1)
+# zwykły apostrof 	'		(przy entrze)
+

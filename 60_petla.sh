47008e34adf042c2d3c54638481de8e0b526b753
Mateusz Adamowski <m.adamowski@alx.pl>
2022-12-10 11:16:43 +0100

    pętla until

---
+++ 60_petla.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+[ -e stop.txt ] && rm stop.txt
+
+echo "Aby przerwać pętlę, utwórz plik stop.txt (w innym terminalu)"
+
+echo "Lub przerwij przez CTRL-C"
+
+until [ -e stop.txt ]
+do
+	date
+	sleep 1
+done
+

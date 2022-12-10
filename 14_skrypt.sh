b3f0663fa2566859a77a33ea7c5ed64c9024105f
Mateusz Adamowski <m.adamowski@alx.pl>
2022-12-10 11:52:52 +0100

    shellcheck wyjątek

--- 14_skrypt.sh
+++ 14_skrypt.sh
@@ -9,8 +9,12 @@
 # gdyby plik był w katalogu domowym
 # source ~/.automatyzator/konfiguracja
 
+# shellcheck disable=SC2154
 echo "imie to: $imie"
+# shellcheck disable=SC2154
 echo "firma to: $firma"
+# shellcheck disable=SC2154
 echo "dzisiaj jest: $dzisiaj"
+# shellcheck disable=SC2154
 echo "pracuję na koncie: $konto"
 

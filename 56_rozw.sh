7694919b86b4bc8255472e7aa52ffce3d2b74076
Mateusz Adamowski <m.adamowski@alx.pl>
2022-12-10 10:54:53 +0100

    zwięzłe warunki

--- 56_rozw.sh
+++ 56_rozw.sh
@@ -19,19 +19,9 @@ do
 		poprawic=$((poprawic + 1))
 	fi
 
-	if grep -q "TODO" "$plik"
-	then
-		todo=tak
-	else
-		todo=brak
-	fi
+	if grep -q "TODO" "$plik" && todo=tak || todo=brak
 
-	if grep -q "FIXME" "$plik"
-	then
-		fixme=tak
-	else
-		fixme=brak
-	fi
+	grep -q "FIXME" "$plik" && fixme=tak || fixme=brak
 
 	echo "$plik: linii kodu: $n (niepustych: $n2), prawa dostępu: $prawa, TODO: $todo, FIXME: $fixme"
 

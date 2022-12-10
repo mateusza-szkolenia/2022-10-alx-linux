eb62441c15503ba31609dd7a4c6aff51f788c27a
Mateusz Adamowski <m.adamowski@alx.pl>
2022-12-10 10:41:31 +0100

    liczba linii (wszystkich i niepustych)

--- 56_rozw.sh
+++ 56_rozw.sh
@@ -4,6 +4,14 @@ for plik in *.sh
 do
 	echo -n "Plik: $plik "
 
+	n=$(wc -l < "$plik")
+
+	echo -n "liczba linii: $n "
+
+	n2=$(cat "$plik" | grep -v '^$' | wc -l)
+
+	echo -n "(niepustych: $n2) "
+
 	if test -x "$plik"
 	then
 		echo "prawa dostępu: OK"

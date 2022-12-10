891d550754813969739efd1a8689075454ded4d3
Mateusz Adamowski <m.adamowski@alx.pl>
2022-12-10 09:43:29 +0100

    zwięzła notacja

--- 55_petla.sh
+++ 55_petla.sh
@@ -2,20 +2,9 @@
 
 for n in 4 17 19 60 23 3 78 34 40 123 13 14 15 16 17 99 8
 do
-	if [ "$n" -gt 100 ]
-	then
-		break
-	fi
-
-	if [ "$n" -gt 50 ]
-	then
-		continue
-	fi
-
-	if [ "$n" -lt 10 ]
-	then
-		continue
-	fi
+	[ "$n" -gt 100 ] && break
+	[ "$n" -gt 50 ] && continue
+	[ "$n" -lt 10 ] && continue
 
 	echo "Liczba: $n"
 	touch "/tmp/$n.txt"

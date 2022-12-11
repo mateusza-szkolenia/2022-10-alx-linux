9da4014ba9d7898a616805cc3d603a33592f7104
Mateusz Adamowski <m.adamowski@alx.pl>
2022-12-11 16:20:50 +0100

    zmienne globalne i lokalne

--- 65_funkcje.sh
+++ 65_funkcje.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+# zmienne są domyślnie globalne
+
 powitanie () {
+	local koniec
 	echo "Witaj"
-	echo "Jest dzisiaj $(date +%F) i pada śnieg"
+	echo "Jest dzisiaj $(date +%F) i $pogoda"
+	koniec=tak
 }
 
 
+koniec=nie
+pogoda='pada śnieg'
+
 powitanie
 powitanie
 powitanie
+
+echo "czy koniec? $koniec"
 

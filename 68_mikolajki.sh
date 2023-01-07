6d4ddb8e042d0949664e9921825fa2aa1ef0322e
Mateusz Adamowski <m.adamowski@alx.pl>
2023-01-07 13:56:52 +0100

    rozwiązanie

--- 68_mikolajki.sh
+++ 68_mikolajki.sh
@@ -3,14 +3,41 @@
 lista='mikolajki.txt'
 
 
-lista_los='mikolajki.txt.shuf'
-lista_par='mikolajki.txt.pary'
+lista_los=$(mktemp /tmp/miko-los.XXXXXXXXXXXX.txt)
+lista_par=$(mktemp /tmp/miko-par.XXXXXXXXXXXX.txt)
 
-shuf "$lista" > "$lista_los"
+function sprawdz_pary () {
+	while read kto komu
+	do
+		# echo "Kto: $kto  komu: $komu"
+		if [[ "$kto" == "$komu" ]]
+		then
+			echo "Błąd! Ktoś wylosował samego siebie! Nie może tak być!"
+			return 1
+		fi
+	done < "$lista_par"
 
-paste "$lista" "$lista_los" > "$lista_par"
+	return 0
+}
 
-echo "Wylosowane pary:"
-cat "$lista_par"
+while true
+do
+	shuf "$lista" > "$lista_los"
+	paste "$lista" "$lista_los" > "$lista_par"
 
+	sprawdz_pary && break
+
+	echo "No niestety, losuję ponownie"
+done
+
+echo "Wysyłam maile:"
+while read kto komu
+do
+	echo "- $kto"
+	# echo "Kupujesz prezent dla: $komu" | mail "$kto"
+	echo "Kupujesz prezent dla: $komu" > "/tmp/$kto.txt"
+done < "$lista_par"
+
+rm "$lista_par"
+rm "$lista_los"
 
